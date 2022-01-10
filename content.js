505b025e60ce86f9394406d7ba5a69c089262f7e
Domcaaaaa <novotny6dominik@gmail.com>
2022-01-10 18:58:49 +0100

    fourth commit

--- content.js
+++ content.js
@@ -2,11 +2,23 @@ let points = $("div.points")
 let grade1=$("span.grade")
 let weigth2=$("div.label")
 let weigth1=$("div.list-item")
+let arrt=$("div.labels-set")
+let arr=[]
+let arlen=arrt.length
 let weights={}
-let grades={} 
+let grades={}
 for (let u = 1; u < weigth1.length ; u++){
     let temp=weigth1[u].innerText.replaceAll(" ","").replace("\n"," ").replace("%","").split(" ");
     weights[temp[0]]=temp[1]
+}
+for(let n=0;n<arlen;n++){
+    try{
+        if(arrt[n].className!=arrt[n+1].className && arrt[n].className=="labels-set flex flex-start flex-wrap"){
+            arr.push(arrt[n].innerText.split("\n")[1].replaceAll(" ",""))
+        }
+    }catch (error){
+
+    }
 }
 let i = points.length
 i=i-1
@@ -18,7 +30,11 @@ while (i > -1) {
     let gradefin=Math.round((100/max)*grade)
     grade1[i].innerHTML=gradefin+"%"
     $("div.points")[i].replaceWith("")
-    console.log($("div.labels-set"));
-    /* console.log($("div.labels-set")[0].innerText,$("div.labels-set")[0+2].innerText); */
+    if (grades.hasOwnProperty(weights[arr[i]])){
+        grades[weights[arr[i]]].push(gradefin)
+    }else{
+        grades[weights[arr[i]]]=[gradefin]
+    }
+    console.log(grades);  
     i=i-1
 }
