9e67b53ac475b9f06a944b1b6531d0bc986f2ae8
Domcaaaaa <novotny6dominik@gmail.com>
2022-01-10 17:58:21 +0100

    third commit

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 let points = $("div.points")
-console.log(points)
+let grade1=$("span.grade")
+let weigth2=$("div.label")
+let weigth1=$("div.list-item")
+let weights={}
+let grades={} 
+for (let u = 1; u < weigth1.length ; u++){
+    let temp=weigth1[u].innerText.replaceAll(" ","").replace("\n"," ").replace("%","").split(" ");
+    weights[temp[0]]=temp[1]
+}
 let i = points.length
 i=i-1
 let regex = /[0-9]{1,3}/g;
@@ -8,7 +16,9 @@ while (i > -1) {
     let grade = pointst[0]
     let max = pointst[1]
     let gradefin=Math.round((100/max)*grade)
-    console.log(gradefin);
-    
-    i = i-1
+    grade1[i].innerHTML=gradefin+"%"
+    $("div.points")[i].replaceWith("")
+    console.log($("div.labels-set"));
+    /* console.log($("div.labels-set")[0].innerText,$("div.labels-set")[0+2].innerText); */
+    i=i-1
 }
